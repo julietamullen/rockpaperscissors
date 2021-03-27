b5949da026b6d7fbcca072c7990affca7403a4df
Julieta Mullen <67779109+julietamullen@users.noreply.github.com>
2021-03-27 05:56:32 -0300

    Add files via upload

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom"
 function App() {
   return (
     <main className="main">
-      <h1>Select:</h1>
+      <h1 className="homeTitle">Select</h1>
     <Link to="../PvP"><button className="select">Player vs. Player</button></Link>
     <Link to="../PvC"><button className="select">Player vs. Computer </button></Link>
     </main>
